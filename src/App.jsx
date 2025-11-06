04ea1f96a2c13f27a6d2a9d145e9143b1f680d4f
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-11-07 01:27:01 +1300

    Complete React migration #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import Header from './components/Header.jsx'
+import Projects from './components/Projects.jsx'
+import Skills from './components/Skills.jsx'
+
 import './App.css'
 
 function App() {
-
-
   return (
-    <div>
-      <h1>Welcome to Nagare Negishi's Portfolio</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+    </>
   )
 }
 
