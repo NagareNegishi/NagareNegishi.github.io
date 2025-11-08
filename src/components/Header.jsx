2a81f85f76d2b093250ed1f52536900bc91f69a5
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-11-08 23:05:29 +1300

    set up and convert header with tailwind #2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { personalInfo } from "../data/personalInfo";
 
 function Header() {
     return (
-        <header>
-            <h1>{ personalInfo.name }</h1>
+        <header className="bg-white py-10 shadow-md text-center">
+            <h1 className="text-4xl mb-2.5">{ personalInfo.name }</h1>
             <p>{ personalInfo.title }</p>
         </header>
     )
