b7bdf9fb21b027f679dc85004283877cc572cf9b
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-13 18:33:24 +1300

    separate ProjectCard into function #8

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import { projects } from '../data/projects';
+import { softwareProjects } from '../data/projects';
 
 function Projects() {
     return (
@@ -6,31 +6,39 @@ function Projects() {
             <div className="container">
                 <h2>Featured Projects</h2>
                 <div className="project-grid">
-                    {projects.map((project) => (
-                        <div className="project-card bg-white p-5 rounded-lg shadow-md" key={project.id}>
-                            <h3>{project.title}</h3>
-                            <div className="project-tags">
-                                {project.tags.map((tag, index) => (
-                                    <span className="tag" key={index}>{tag}</span>
-                                ))}
-                            </div>
-                            <p>{project.description}</p>
-                            <div className="flex gap-[15px]">
-                                {project.links.demo && (
-                                    <a className="text-[#0066cc] no-underline hover:underline"
-                                        href={project.links.demo}>{project.links.demoLabel}</a>
-                                )}
-                                {project.links.github && (
-                                    <a className="text-[#0066cc] no-underline hover:underline"
-                                        href={project.links.github}>View Code</a>
-                                )}
-                            </div>
-                        </div>
+                    {softwareProjects.map((project) => (
+                        <ProjectCard key={project.id} project={project} />
                     ))}
                 </div>
             </div>
         </section>
     )
 }
+
+
+function ProjectCard({ project }) {
+    return (
+        <div className="project-card bg-white p-5 rounded-lg shadow-md">
+            <h3>{project.title}</h3>
+            <div className="project-tags">
+                {project.tags.map((tag, index) => (
+                    <span className="tag" key={index}>{tag}</span>
+                ))}
+            </div>
+            <p>{project.description}</p>
+            <div className="flex gap-[15px]">
+                {project.links.demo && (
+                    <a className="text-[#0066cc] no-underline hover:underline"
+                        href={project.links.demo}>{project.links.demoLabel}</a>
+                )}
+                {project.links.github && (
+                    <a className="text-[#0066cc] no-underline hover:underline"
+                        href={project.links.github}>View Code</a>
+                )}
+            </div>
+        </div>
+    );
+}
+
 
 export default Projects;
