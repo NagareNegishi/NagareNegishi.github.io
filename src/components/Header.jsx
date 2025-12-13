58a0bbfdf4b9625fe413272b34db69f7ca27c511
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-13 17:56:50 +1300

    separate project into 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { personalInfo } from "../data/personalInfo";
 function Header() {
     return (
         <header className="bg-white py-10 shadow-md text-center">
-            <h1 className="text-4xl mb-2.5 font-bold">{ personalInfo.name }</h1>
-            <p>{ personalInfo.title }</p>
+            <h1 className="text-5xl mb-5.5 font-bold">{ personalInfo.name }</h1>
+            <p className="text-xl ">{ personalInfo.title }</p>
         </header>
     )
 }
