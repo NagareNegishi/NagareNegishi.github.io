5c7df36046e27857b3ba5c859cfe427777a9955d
Nagare Negishi <negishinaga@gmail.com>
2026-06-23 23:11:42 +1200

    ectionCard #24

---
+++ src/components/SectionCard.jsx
@@ -0,0 +1,9 @@
+function SectionCard({ children }) {
+    return (
+        <div className="max-w-[1200px] mx-auto px-8 bg-white rounded-xl shadow-sm">
+            {children}
+        </div>
+    )
+}
+
+export default SectionCard
