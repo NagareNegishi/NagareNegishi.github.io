1efdc3d9583da16291a11a2e82a9a3799be03247
Nagare Negishi <negishinaga@gmail.com>
2026-08-18 21:37:18 +1200

    WhatIBring #28

---
+++ src/components/WhatIBring.jsx
@@ -0,0 +1,23 @@
+import { strengths } from "../data/strengths";
+import SectionCard from "./SectionCard";
+import SectionHeading from "./SectionHeading";
+
+function WhatIBring() {
+    return (
+        <section id="what-i-bring" className="py-8 md:py-12 scroll-mt-16">
+            <SectionCard>
+                <SectionHeading>What I Bring</SectionHeading>
+                <ul className="flex flex-col gap-4 mt-5">
+                    {strengths.map((text, index) => (
+                        <li key={index} className="flex gap-3">
+                            <span className="text-green-600 font-semibold shrink-0 mt-0.5">✓</span>
+                            <span>{text}</span>
+                        </li>
+                    ))}
+                </ul>
+            </SectionCard>
+        </section>
+    )
+}
+
+export default WhatIBring
